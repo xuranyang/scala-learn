ba82d1cc5a1872d8428138bada26ecb0f302656e
xry <362438997@qq.com>
2024-02-02 01:26:45 +0800

    新增SparkCore

---
+++ src/main/scala/com/bigdata/WordCount.scala
@@ -0,0 +1,30 @@
+package com.bigdata
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+    def main(args: Array[String]): Unit = {
+        // 创建 SparkConf 对象
+        val conf = new SparkConf()
+          .setAppName("WordCount")
+          .setMaster("local[*]")
+
+        // 创建 SparkContext 对象
+        val sc = new SparkContext(conf)
+
+        // 读取文本文件
+        val lines = sc.textFile("src/main/resources/input.txt")
+
+        // 执行 WordCount
+        val wordCounts = lines
+          .flatMap(line => line.split("\\s+"))
+          .map(word => (word, 1))
+          .reduceByKey(_ + _)
+
+        // 输出结果
+        wordCounts.collect().foreach(println)
+
+        // 停止 SparkContext
+        sc.stop()
+    }
+}
